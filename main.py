- Fixed precision - Added "Automatic Mixed Precision"

main.py
@@ -19,6 +19,7 @@ def prepare_dataset() -> DataLoader:
     dataset_dict = deepcopy(config_dict)["dataset"]
     dataset_dict["prev_samples"] = config_dict["prev_samples"]
     dataset_dict["future_samples"] = config_dict["future_samples"]
+    dataset_dict["precision"] = np.float16 if config_dict["device"] == "cuda" else np.float32
     loader_dict = dataset_dict["loader"]
     del dataset_dict["loader"]
 
@@ -39,6 +40,7 @@ def prepare_model() -> BaseModel:
     model_name = model_dict["model"]["name"]
     model_dict["model"]["cache_size"] = model_dict["prev_samples"]
     model_dict["model"]["log"] = model_dict["log"]
+    model_dict["precision"] = np.float16 if config_dict["device"] == "cuda" else np.float32
     del model_dict["model"]["name"]
     del model_dict["model"]["train"]
 
@@ -54,8 +56,6 @@ def prepare_model() -> BaseModel:
         to_store["log"] = False
         to_store["evaluation"] = model.log_path
         to_store["device"] = "cpu"
-        # FIXME: fix precision handling
-        # del to_store["dataset"]["precision"]
         config.store_args(f"{model.log_path}/config.yml", to_store)
     
     else:
@@ -92,7 +92,6 @@ def train_mode():
 def load_mode():
     # disable some configurations, such as shuffle to gain a useful output
     config_dict["log"] = False
-    config_dict["dataset"]["loader"]["batch_size"] = 1
     config_dict["dataset"]["loader"]["shuffle"] = False
     
     # prepare the dataset and model
@@ -105,9 +104,11 @@ def load_mode():
     wave = []
     for X, y in tqdm(dataloader):
         X_midi, _ = X
-        window = model.predict(X_midi)
-        for sample in window:
-            wave.append(sample)
+        batch_size, _, _ = X_midi.shape
+        for sample in range(batch_size):
+            window = model.predict(X_midi[sample])
+            for sample in window:
+                wave.append(sample)
 
     wave = np.array(wave)
     print(wave, wave.shape)
@@ -128,8 +129,6 @@ if __name__ == "__main__":
 
     # load model arguments and check if it can be evaluated
     config_dict = config.get_args(args.config)
-    # FIXME
-    # config_dict["dataset"]["precision"] = np.float16 if config_dict["device"] == "cuda" else np.float32
     log_path = config_dict["evaluation"]
 
     if args.load_only:

model/base.py
@@ -15,7 +15,7 @@ def no_autocast():
 
 
 class BaseModel(nn.Module):
-    def __init__(self, tag: str, log: bool = True) -> None:
+    def __init__(self, tag: str, log: bool = True, precision: torch.dtype = torch.float32) -> None:
         """
         Base class of a model. This only includes method-playeholders 
         and general methods for statistics etc.
@@ -44,6 +44,10 @@ class BaseModel(nn.Module):
         # input buffer from previous wave form
         self._n_channels = 2
         self._cache = None
+
+        # the following is needed to avoid zero gradients in mixed precision
+        self._precision = precision
+        self.__scaler = torch.cuda.amp.GradScaler()
     
     @property
     def channels(self) -> int:
@@ -132,11 +136,14 @@ class BaseModel(nn.Module):
                 y = y.to(self.device)
 
                 # train a batch
+                with torch.autocast(device_type=self.device, dtype=self._precision):
+                    pred_y = self((X_midi, X_wave))
+                    loss = self._loss_fn(pred_y, y)
+
+                self.__scaler.scale(loss).backward()
+                self.__scaler.step(self._optim)
+                self.__scaler.update()
                 self._optim.zero_grad()
-                pred_y = self((X_midi, X_wave))
-                loss = self._loss_fn(pred_y, y)
-                loss.backward()
-                self._optim.step()
 
                 # log for the statistics
                 self._writer.add_scalar("Train/loss", loss.item(), self._sample_position)
@@ -166,6 +173,7 @@ class BaseModel(nn.Module):
     def predict(self, midi) -> List:
         # move the input onto the same device as the model is on
         midi = midi.to(self.device)
+        midi = torch.unsqueeze(midi, 0)
 
         # calculating a gradient is not needed, disable that to improve performance
         with torch.no_grad():

model/cnn_model.py
@@ -11,10 +11,10 @@ from torch.optim.lr_scheduler import ExponentialLR
 class CnnModel(BaseModel):
     def __init__(self, tag: str, channels: int = 2, lr: float = 1e-3, lr_decay: float = 9e-1, 
                  adam_betas: List[float] = [9e-1, 999e-3], input_sequence: int = 1, 
-                 output_sequence: int = 1, log: bool = True) -> None:
+                 output_sequence: int = 1, log: bool = True, precision: torch.dtype = torch.float32) -> None:
 
         # initialize components using the parent class
-        super(CnnModel, self).__init__(tag, log)
+        super(CnnModel, self).__init__(tag, log, precision)
 
         # define hyperparameters for the network itself
         self.__channels = channels

model/lstm_model.py
@@ -11,9 +11,9 @@ from torch.optim.lr_scheduler import ExponentialLR
 class LstmModel(BaseModel):
     def __init__(self, tag: str, channels: int = 2, lr: float = 1e-3, lr_decay: float = 9e-1, 
                  adam_betas: List[float] = [9e-1, 999e-3], cache_size: int = 1,
-                 log: bool = True) -> None:
+                 log: bool = True, precision: torch.dtype = torch.float32) -> None:
         # initialize components using the parent class
-        super(LstmModel, self).__init__(tag, log)
+        super(LstmModel, self).__init__(tag, log, precision)
 
         # define hyperparameters for the network itself
         self.__channels = channels
@@ -49,7 +49,7 @@ class LstmModel(BaseModel):
         self._loss_fn = torch.nn.L1Loss()
         self._optim = torch.optim.AdamW(self.parameters(), lr=lr, betas=adam_betas)
         self._scheduler = ExponentialLR(self._optim, gamma=lr_decay)
-        self._cache = torch.zeros((cache_size, 2), dtype=torch.float32)
+        self._cache = torch.zeros((cache_size, 2), dtype=self._precision)
     
     def load(self, path) -> None:
         """Loads the model's parameter given a path